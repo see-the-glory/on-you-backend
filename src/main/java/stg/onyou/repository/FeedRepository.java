745456c4facf05b4a8afa744b993a7f1492410dd
jaekwang <ajaekwang@gmail.com>
2022-11-15 23:07:49 +0900

    selectFeed order by created desc

--- src/main/java/stg/onyou/repository/FeedRepository.java
+++ src/main/java/stg/onyou/repository/FeedRepository.java
@@ -9,10 +9,10 @@ import java.util.List;
 
 public interface FeedRepository extends JpaRepository<Feed, Long> {
 
-    @Query("select f from Feed f where f.delYn = 'n' and f.reportCount < 5 and f.access = 'PUBLIC'")
+    @Query("select f from Feed f where f.delYn = 'n' and f.reportCount < 5 and f.access = 'PUBLIC' order by f.created desc")
     List<Feed> findAll();
 
     @Query("select f from Feed f where f.delYn = 'n' and f.reportCount < 5 and f.content like concat('%', :content, '%') " +
-            "or f.content like concat('%', '#', :hashtag, '%')")
+            "or f.content like concat('%', '#', :hashtag, '%') order by f.created desc")
     List<Feed> findAllString(@Param("content") String content, @Param("hashtag") String hashtag);
 }
