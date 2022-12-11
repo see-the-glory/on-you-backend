0669df6102e87e9cdff8f27edb78715611b9959c
Junyong Jang <aszx6992@naver.com>
2022-12-11 22:33:19 +0900

    [Fix] UserNotification Bean 에러, myClub API 스펙 수정( applyStatus 추가, members 리스트 제거 )

--- src/main/java/stg/onyou/repository/UserNotificationQRepositoryImpl.java
+++ src/main/java/stg/onyou/repository/UserNotificationQRepositoryImpl.java
@@ -3,6 +3,7 @@ package stg.onyou.repository;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
+import org.springframework.stereotype.Repository;
 import stg.onyou.model.entity.UserNotification;
 import stg.onyou.model.network.response.QUserNotificationResponse;
 import stg.onyou.model.network.response.UserNotificationResponse;
@@ -12,6 +13,7 @@ import java.util.List;
 import static stg.onyou.model.entity.QUserNotification.userNotification;
 import static stg.onyou.model.entity.QAction.action;
 
+@Repository
 public class UserNotificationQRepositoryImpl extends QuerydslRepositorySupport implements UserNotificationQRepository{
 
     private final JPAQueryFactory queryFactory;
--- src/main/java/stg/onyou/service/NotificationService.java
+++ src/main/java/stg/onyou/service/NotificationService.java
@@ -1,40 +1,28 @@
-//package stg.onyou.service;
-//
-//import org.modelmapper.ModelMapper;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Service;
-//import stg.onyou.exception.CustomException;
-//import stg.onyou.exception.ErrorCode;
-//import stg.onyou.model.entity.UserNotification;
-//import stg.onyou.model.network.Header;
-//import stg.onyou.model.network.response.UserNotificationResponse;
-//import stg.onyou.repository.UserNotificationQRepository;
-//import stg.onyou.repository.UserNotificationRepository;
-//
-//import javax.transaction.Transactional;
-//import java.util.List;
-//import java.util.stream.Collectors;
-//
-//@Service
-//@Transactional
-//public class NotificationService {
-//
-//    @Autowired
-//    private UserNotificationRepository userNotificationRepository;
-//
-//    @Autowired
-//    private UserNotificationQRepository userNotificationQRepository;
-//
-//    @Autowired
-//    private ModelMapper modelMapper;
-//
-//    /**
-//     * 전체 카테고리 select
-//     */
-//    public Header<List<UserNotificationResponse>> selectUserNotification(Long userId){
-//
-//        List<UserNotificationResponse> userNotificationList = userNotificationQRepository.findUserNotificationList(userId);
-//        return Header.OK(userNotificationList);
-//    }
-//
-//}
+package stg.onyou.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import stg.onyou.model.network.Header;
+import stg.onyou.model.network.response.UserNotificationResponse;
+import stg.onyou.repository.UserNotificationQRepositoryImpl;
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service
+@Transactional
+public class NotificationService {
+
+    @Autowired
+    private UserNotificationQRepositoryImpl userNotificationQRepositoryImpl;
+
+
+    /**
+     * 전체 카테고리 select1`
+     */
+    public Header<List<UserNotificationResponse>> selectUserNotification(Long userId){
+
+        List<UserNotificationResponse> userNotificationList = userNotificationQRepositoryImpl.findUserNotificationList(userId);
+        return Header.OK(userNotificationList);
+    }
+
+}
