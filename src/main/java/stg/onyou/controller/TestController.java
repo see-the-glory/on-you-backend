01e240316f73ae306d07a7dcbae165d26771b0bd
Junyong Jang <aszx6992@naver.com>
2023-02-03 22:16:14 +0900

    [Add] FirebaseCloudMessaging 구현 및 test api 구현

--- src/main/java/stg/onyou/controller/TestController.java
+++ src/main/java/stg/onyou/controller/TestController.java
@@ -1,17 +1,42 @@
 package stg.onyou.controller;
 
 import io.swagger.annotations.Api;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import stg.onyou.service.FirebaseCloudMessageService;
+
+import java.io.IOException;
 
 @Api(tags = {"Test API Controller"})
 @Slf4j
 @RestController
+@RequiredArgsConstructor
 public class TestController {
+
+    private final FirebaseCloudMessageService firebaseCloudMessageService;
+
+    @PostMapping("/api/test/fcm")
+    public ResponseEntity pushMessage(@RequestBody TestDTO requestDTO) throws IOException {
+
+        System.out.println(requestDTO.getTargetToken() + " "
+                +requestDTO.getTitle() + " " + requestDTO.getBody());
+
+        firebaseCloudMessageService.sendMessageTo(
+                requestDTO.getTargetToken(),
+                requestDTO.getTitle(),
+                requestDTO.getBody());
+
+        return ResponseEntity.ok().build();
+    }
 
     @PostMapping("/test")
     public String test() {
         return "test success!";
     }
+
+
 }
---
+++ src/main/java/stg/onyou/controller/TestDTO.java
@@ -0,0 +1,13 @@
+package stg.onyou.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class TestDTO {
+
+    private String title;
+    private String targetToken;
+    private String body;
+}
